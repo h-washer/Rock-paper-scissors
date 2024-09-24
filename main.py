a8f3c091d2884597ffa3e5bc57a2afaacd7d0630
h-washer <harvey.washer@students.plymouth.ac.uk>
2024-09-24 14:41:30 +0000

    Added loop

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-#simple.py
+# loop.py
 import random
 
 CHOICES = ["rock", "paper", "scissors"]
 
-print("Make your throw")
+while True:
+    print("Make your throw")
+    user_choice = input("   Type rock, paper, or scissors:  ")
+    if user_choice in CHOICES:
+        computer_choice = random.choice(CHOICES)
+        print(
+            f"\nYou threw '{user_choice}', the computer threw '{computer_choice}'"
+      )
+    else:
+      print(f"\nYou typed '{user_choice}' which isn't a valid throw")
 
-user_choice = input("   Type rock, paper, or scissors:  ")
+    again = input("\nWould you like to play again? (y/n): ")
+    if again.lower() == "n":
+        break
 
-if user_choice in CHOICES:
-  computer_choice = random.choice(CHOICES)
-  print(
-    f"\nYou threw '{user_choice}', the computer threw '{computer_choice}'"
-  )
-else:
-  print(f"\nYou typed '{user_choice}' which isn't a valid throw")
+    print()
+
+print("\nGoodbye!")
 
